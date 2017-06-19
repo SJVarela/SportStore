86e4a4a1582ab7e51e0a99b1af6ed706edfa2eac
Santa <s.javiervarela@gmail.com>
2017-06-19 17:39:22 -0300

    SetUp

---
+++ SportStore/SportStore.WebUI/Infraestructure/NinjectDependencyResolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Ninject;
+namespace SportStore.WebUI.Infraestructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            //Bindings goes here
+        }
+    }
+}
