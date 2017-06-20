8df54fd7bf40462cc5bab7e0db598d25621c3303
Santa <s.javiervarela@gmail.com>
2017-06-20 15:57:44 -0300

    First Cart Implementation

---
+++ SportStore/SportStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+using SportStore.WebUI.Models;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductsRepository repository;
+        public CartController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel { Cart = GetCart(), ReturnUrl = returnUrl });
+        }
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            var product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if(product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            var product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if(product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            var cart = (Cart)Session["Cart"];
+            if(cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+    
+}
