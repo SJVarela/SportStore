c3b8d604ac2529bb2c6a2aebb7a27924d37f105f
Santiago Varela <s.javiervarela@gmail.com>
2017-06-20 11:29:20 -0300

    Continuing

--- SportStore/SportStore.UnitTests/UnitTest1.cs
+++ SportStore/SportStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+using SportStore.WebUI.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using SportStore.WebUI.Models;
+using SportStore.WebUI.HtmlHelpers;
+
+using Moq;
 
 namespace SportStore.UnitTests
 {
@@ -7,8 +17,42 @@ namespace SportStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            // Arrange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] 
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            // Act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            // Assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            HtmlHelper myHelper = null;
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>" + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>" + @"<a class=""btn btn-default"" href=""Page3"">3</a>", result.ToString());
         }
     }
 }
--- SportStore/SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore/SportStore.WebUI/Controllers/ProductController.cs
@@ -5,19 +5,34 @@ using System.Web;
 using System.Web.Mvc;
 using SportStore.Domain.Entities;
 using SportStore.Domain.Abstract;
+using SportStore.WebUI.Models;
 namespace SportStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         // GET: Product
         private IProductsRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductsRepository productRepository)
         {
             this.repository = productRepository;
         }
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            var model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
